346096892e8fa11c36ef06cf03899c6fc5bbad7a
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-11-13 16:12:59 -0300

    add initial setup for auth module

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './components/register/register.component';
+import { RouterModule } from '@angular/router';
+
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  exports: [RegisterComponent],
+})
+export class AuthModule {}
