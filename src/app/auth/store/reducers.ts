cbc48cac2435b9a34ee4c11c54d417c46dea4478
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-11-22 00:08:27 -0300

    update auth reducers

--- src/app/auth/store/reducers.ts
+++ src/app/auth/store/reducers.ts
@@ -1,16 +1,44 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import { AuthStateInterface } from 'src/app/auth/types/auth-state.interface';
-import { registerAction } from 'src/app/auth/store/actions/register.action';
+import {
+  registerAction,
+  registerFailureAction,
+  registerSuccessAction,
+} from 'src/app/auth/store/actions/register.action';
 
 const initialState: AuthStateInterface = {
   isSubmitting: false,
+  currentUser: null,
+  validationErrors: null,
+  isLoggedIn: null,
 };
 
 const authReducer = createReducer(
   initialState,
   on(
     registerAction,
-    (state): AuthStateInterface => ({ ...state, isSubmitting: true })
+    (state): AuthStateInterface => ({
+      ...state,
+      isSubmitting: true,
+      validationErrors: null,
+    })
+  ),
+  on(
+    registerSuccessAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      isLoggedIn: true,
+      currentUser: action.currentUser,
+    })
+  ),
+  on(
+    registerFailureAction,
+    (state, action): AuthStateInterface => ({
+      ...state,
+      isSubmitting: false,
+      validationErrors: action.errors,
+    })
   )
 );
 
