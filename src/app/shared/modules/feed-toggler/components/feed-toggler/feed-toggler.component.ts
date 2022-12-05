140c08063de90d1944dfde26b7efcdc613144184
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-12-04 21:38:54 -0300

    add feed toggler

---
+++ src/app/shared/modules/feed-toggler/components/feed-toggler/feed-toggler.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { isLoggedInSelector } from 'src/app/auth/store/selectors';
+import { AppStateInterface } from 'src/app/shared/types/app-state.interface';
+
+@Component({
+  selector: 'mc-feed-toggler',
+  templateUrl: './feed-toggler.component.html',
+  styleUrls: ['./feed-toggler.component.scss'],
+})
+export class FeedTogglerComponent implements OnInit {
+  @Input()
+  tagName = '';
+
+  isLoggedIn$!: Observable<boolean | null>;
+
+  constructor(private store: Store<AppStateInterface>) {}
+
+  ngOnInit(): void {
+    this.initializeValues();
+  }
+
+  initializeValues() {
+    this.isLoggedIn$ = this.store.pipe(select(isLoggedInSelector));
+  }
+}
