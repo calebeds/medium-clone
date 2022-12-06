7f78ca34c2fb71618e14acd94915fa6a2c31d4d5
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-12-06 17:49:05 -0300

    render article page

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -4,10 +4,26 @@ import { ArticleService } from '../shared/services/article/article.service';
 import { ArticleComponent } from './components/article/article.component';
 import { EffectsModule } from '@ngrx/effects';
 import { GetArticleEffect } from './store/effects/get-article.effect';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './store/reducers';
+import { RouterModule } from '@angular/router';
+import { LoadingModule } from '../shared/modules/loading/loading.module';
+import { ErrorMessageModule } from '../shared/modules/error-message/error-message.module';
+import { TagListModule } from '../shared/modules/tag-list/tag-list.module';
+
+const routes = [{ path: 'articles/:slug', component: ArticleComponent }];
 
 @NgModule({
   declarations: [ArticleComponent],
-  imports: [CommonModule, EffectsModule.forFeature([GetArticleEffect])],
+  imports: [
+    CommonModule,
+    EffectsModule.forFeature([GetArticleEffect]),
+    StoreModule.forFeature('article', reducers),
+    RouterModule.forChild(routes),
+    LoadingModule,
+    ErrorMessageModule,
+    TagListModule,
+  ],
   providers: [ArticleService],
 })
 export class ArticleModule {}
--- src/app/article/components/article/article.component.ts
+++ src/app/article/components/article/article.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { combineLatest, Observable, pipe, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { currentUserSelector } from 'src/app/auth/store/selectors';
+import { AppStateInterface } from 'src/app/shared/types/app-state.interface';
+import { ArticleInterface } from 'src/app/shared/types/article.interface';
+import { CurrentUserInterface } from 'src/app/shared/types/current-user.interface';
+import { getArticleAction } from '../../store/actions/get-article.action';
+import {
+  articleSelector,
+  errorSelector,
+  isLoadingSelector,
+} from '../../store/selectors';
 
 @Component({
-  selector: 'app-article',
+  selector: 'mc-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
-export class ArticleComponent implements OnInit {
+export class ArticleComponent implements OnInit, OnDestroy {
+  slug!: string | null;
+  article!: ArticleInterface;
+  articleSubscription!: Subscription;
+  isLoading$!: Observable<boolean>;
+  error$!: Observable<string | null>;
+  isAuthor$!: Observable<boolean>;
 
-  constructor() { }
+  constructor(
+    private store: Store<AppStateInterface>,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.initializeValues();
+    this.initializeListeneres();
+    this.fetchData();
   }
 
+  ngOnDestroy(): void {
+    this.articleSubscription.unsubscribe();
+  }
+
+  initializeValues(): void {
+    this.slug = this.route.snapshot.paramMap.get('slug');
+    this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+    this.error$ = this.store.pipe(select(errorSelector));
+
+    this.isAuthor$ = combineLatest([
+      this.store.pipe(select(articleSelector)),
+      this.store.pipe(select(currentUserSelector)),
+    ]).pipe(
+      map(
+        ([article, currentUser]: [
+          ArticleInterface | null,
+          CurrentUserInterface | null
+        ]) => {
+          if (!article || !currentUser) {
+            return false;
+          }
+          return currentUser.username === article.author.username;
+        }
+      )
+    );
+  }
+
+  initializeListeneres() {
+    this.articleSubscription = this.store
+      .pipe(select(articleSelector))
+      .subscribe((article: ArticleInterface | null) => {
+        this.article = <ArticleInterface>article;
+      });
+  }
+
+  fetchData(): void {
+    this.store.dispatch(getArticleAction({ slug: <string>this.slug }));
+  }
 }
